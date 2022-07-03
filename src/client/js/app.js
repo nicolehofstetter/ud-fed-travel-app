2080ff274cc217395b9adfe2fe5319ffaef30389
Nicole Hofstetter <nicole.hofstetter@audi.de>
2022-07-03 15:18:01 +0200

    Added CountDown and Travel duration

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -43,19 +43,32 @@ const getCoordinatesByCity = async (city) => {
     }
 };
 
+function calculateTravelLength(start, end) {
+    let startDate = new Date(start);
+    let endDate = new Date(end);
+    let period = endDate.getTime() - startDate.getTime();
+    return Math.round(period / (1000 * 60 * 60 * 24));
+}
+
+
 const updateUiWithRecentData = async () => {
     const response = await fetch('http://localhost:8081/api/travels/latest');
     try {
-        const latestTravelEntry = await response.json();
-        console.log(latestTravelEntry);
-        document.getElementById('cityOutput').innerHTML = latestTravelEntry.city;
-        document.getElementById('startOutput').innerHTML = latestTravelEntry.startDate;
-        document.getElementById('endOutput').innerHTML = latestTravelEntry.endDate;
-        document.getElementById('temperatureOutput').innerHTML = latestTravelEntry.temperature;
+        const lastTravelDate = await response.json();
+        console.log(lastTravelDate);
+        document.getElementById('cityOutput').innerHTML = lastTravelDate.city;
+        document.getElementById('startOutput').innerHTML = lastTravelDate.startDate;
+        document.getElementById('endOutput').innerHTML = lastTravelDate.endDate;
+        document.getElementById('temperatureOutput').innerHTML = lastTravelDate.temperature;
 
+        const days = calculateTravelLength(lastTravelDate.startDate, lastTravelDate.endDate);
+        const countDown = calculateTravelLength(new Date().getTime(), lastTravelDate.startDate);
+
+        document.getElementById('travelLength').innerHTML = days;
+        document.getElementById('countDown').innerHTML = countDown;
         const corsImageModified = new Image();
         corsImageModified.crossOrigin = 'Anonymous';
-        corsImageModified.src = latestTravelEntry.imageUrl + '?not-from-cache-please';
+        corsImageModified.src = lastTravelDate.imageUrl + '?not-from-cache-please';
         document.getElementById('cityImage').append(corsImageModified);
 
     } catch (error) {
